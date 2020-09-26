4e6716294796530f32c458ce90367c179b5b3e29
yxj0312 <yanxiajun@hotmail.com>
2020-09-26 23:37:29 +0200

    Install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+import autoprefixer from 'autoprefixer';
+
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+  ],
+};
