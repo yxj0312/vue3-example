036558664e25feebaec2f8578e271c34a4c6b6e8
yxj0312 <yanxiajun@hotmail.com>
2020-10-05 23:20:24 +0200

    Extract List item

---
+++ src/components/ListItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <router-link :to="url">
+    <div class="flex">
+      <div class="flex-shrink-0">
+        <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+          <!-- Heroicon name: scale -->
+          <slot name="icon" />
+        </div>
+      </div>
+      <div class="ml-4">
+        <h4 class="text-lg leading-6 font-medium text-gray-900">
+          {{ title }}
+        </h4>
+        <p class="mt-2 text-base leading-6 text-gray-500">
+          <slot name="description" />
+        </p>
+      </div>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'ListItem',
+  props: {
+    url: {
+      type: Object,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
